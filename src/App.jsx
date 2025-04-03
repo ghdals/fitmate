3cbc436b049ae3a5ccc4fedc97d42b0cdc217e5f
한승제 <takh@MacBook-Air.local>
2025-04-03 13:39:48 +0900

    Update Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AppRoutes from "./routes/AppRoutes";
 import NavBar from './components/NavBar';
@@ -8,7 +5,7 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="w-full min-h-screen bg-black">
       <NavBar />
       <div className="flex flex-col items-center justify-center min-h-screen">
         <AppRoutes />
